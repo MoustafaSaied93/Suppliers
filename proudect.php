2eb09cfff7f1137001aa290534761618b33d4957
Gharib2020678 <69073633+Gharib2020678@users.noreply.github.com>
2021-08-20 12:44:16 +0300

    up

--- proudect.php
+++ proudect.php
@@ -285,9 +285,9 @@
 			</div>
 			<div class="row">
 				<div class="col-lg-6">
-					<div class="breadcrumb_content style2 mb0-991">
+					<div class="breadcrumb_content style2 mb0-991" style="text-align: right;">
 						<ol class="breadcrumb">
-						    <li class="breadcrumb-item"><a href="#">Home</a></li>
+						    <li class="breadcrumb-item"><a href="#">الريئسية</a></li>
 						    <li class="breadcrumb-item active text-thm" aria-current="page">قائمة المنتجات</li>
 						</ol>
 						<h2 class="breadcrumb_title">قائمة المنتجات</h2>
@@ -295,13 +295,13 @@
 				</div>
 				<div class="col-lg-6">
 					<div class="listing_list_style mb20-xsd tal-991">
-						<ul class="mb0">
+						<ul class="mb0" style="float: left;">
 							<li class="list-inline-item"><a href="#"><span class="fa fa-th-large"></span></a></li>
 							<li class="list-inline-item"><a href="#"><span class="fa fa-th-list"></span></a></li>
 						</ul>
 					</div>
-					<div class="dn db-991 mt30 mb0">
-						<div id="main2">
+					<div class="dn db-991 mt30 mb0" >
+						<div id="main2" >
 							<span id="open2" class="flaticon-filter-results-button filter_open_btn style2"> Show Filter</span>
 						</div>
 					</div>
@@ -313,275 +313,29 @@
 						<div class="sidebar_listing_list">
 							<div class="sidebar_advanced_search_widget">
 								<ul class="sasw_list mb0">
-									<li class="search_area">
-									    <div class="form-group">
-									    	<input type="text" class="form-control" id="exampleInputName1" placeholder="keyword">
-									    	<label for="exampleInputEmail"><span class="flaticon-magnifying-glass"></span></label>
-									    </div>
-									</li>
-									<li class="search_area">
-									    <div class="form-group">
-									    	<input type="text" class="form-control" id="exampleInputEmail" placeholder="Location">
-									    	<label for="exampleInputEmail"><span class="flaticon-maps-and-flags"></span></label>
-									    </div>
-									</li>
-									<li>
-										<div class="search_option_two">
-											<div class="candidate_revew_select">
-												<select class="selectpicker w100 show-tick">
-													<option>Status</option>
-													<option>Apartment</option>
-													<option>Bungalow</option>
-													<option>Condo</option>
-													<option>House</option>
-													<option>Land</option>
-													<option>Single Family</option>
-												</select>
-											</div>
-										</div>
-									</li>
-									<li>
-										<div class="search_option_two">
-											<div class="candidate_revew_select">
-												<select class="selectpicker w100 show-tick">
-													<option>Property Type</option>
-													<option>Apartment</option>
-													<option>Bungalow</option>
-													<option>Condo</option>
-													<option>House</option>
-													<option>Land</option>
-													<option>Single Family</option>
-												</select>
-											</div>
-										</div>
-									</li>
-									<li>
-										<div class="small_dropdown2">
-										    <div id="prncgs2" class="btn dd_btn">
-										    	<span>Price</span>
-										    	<label for="exampleInputEmail2"><span class="fa fa-angle-down"></span></label>
-										    </div>
-										  	<div class="dd_content2">
-											    <div class="pricing_acontent">
-													<input type="text" class="amount" placeholder="$52,239"> 
-													<input type="text" class="amount2" placeholder="$985,14">
-													<div class="slider-range"></div>
-											    </div>
-										  	</div>
-										</div>
-									</li>
-									<li>
-										<div class="search_option_two">
-											<div class="candidate_revew_select">
-												<select class="selectpicker w100 show-tick">
-													<option>Bathrooms</option>
-													<option>1</option>
-													<option>2</option>
-													<option>3</option>
-													<option>4</option>
-													<option>5</option>
-													<option>6</option>
-												</select>
-											</div>
-										</div>
-									</li>
-									<li>
-										<div class="search_option_two">
-											<div class="candidate_revew_select">
-												<select class="selectpicker w100 show-tick">
-													<option>Bedrooms</option>
-													<option>1</option>
-													<option>2</option>
-													<option>3</option>
-													<option>4</option>
-													<option>5</option>
-													<option>6</option>
-												</select>
-											</div>
-										</div>
-									</li>
-									<li>
-										<div class="search_option_two">
-											<div class="candidate_revew_select">
-												<select class="selectpicker w100 show-tick">
-													<option>Garages</option>
-													<option>Yes</option>
-													<option>No</option>
-													<option>Others</option>
-												</select>
-											</div>
-										</div>
-									</li>
-									<li>
-										<div class="search_option_two">
-											<div class="candidate_revew_select">
-												<select class="selectpicker w100 show-tick">
-													<option>Year built</option>
-													<option>2013</option>
-													<option>2014</option>
-													<option>2015</option>
-													<option>2016</option>
-													<option>2017</option>
-													<option>2018</option>
-													<option>2019</option>
-													<option>2020</option>
-												</select>
-											</div>
-										</div>
-									</li>
-									<li class="min_area list-inline-item">
-									    <div class="form-group">
-									    	<input type="text" class="form-control" id="exampleInputName2" placeholder="Min Area">
-									    </div>
-									</li>
-									<li class="max_area list-inline-item">
-									    <div class="form-group">
-									    	<input type="text" class="form-control" id="exampleInputName3" placeholder="Max Area">
-									    </div>
-									</li>
-									<li>
-									  	<div id="accordion" class="panel-group">
-										    <div class="panel">
-										      	<div class="panel-heading">
-											      	<h4 class="panel-title">
-											        	<a href="#panelBodyRating" class="accordion-toggle link" data-toggle="collapse" data-parent="#accordion"><i class="flaticon-more"></i> Advanced features</a>
-											        </h4>
-										      	</div>
-											    <div id="panelBodyRating" class="panel-collapse collapse">
-											        <div class="panel-body row">
-											      		<div class="col-lg-12">
-											                <ul class="ui_kit_checkbox selectable-list float-left fn-400">
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck16">
-																		<label class="custom-control-label" for="customCheck16">Air Conditioning</label>
-																	</div>
-											                	</li>
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck17">
-																		<label class="custom-control-label" for="customCheck17">Barbeque</label>
-																	</div>
-											                	</li>
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck18">
-																		<label class="custom-control-label" for="customCheck18">Gym</label>
-																	</div>
-											                	</li>
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck19">
-																		<label class="custom-control-label" for="customCheck19">Microwave</label>
-																	</div>
-											                	</li>
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck20">
-																		<label class="custom-control-label" for="customCheck20">TV Cable</label>
-																	</div>
-											                	</li>
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck21">
-																		<label class="custom-control-label" for="customCheck21">Lawn</label>
-																	</div>
-											                	</li>
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck22">
-																		<label class="custom-control-label" for="customCheck22">Refrigerator</label>
-																	</div>
-											                	</li>
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck23">
-																		<label class="custom-control-label" for="customCheck23">Swimming Pool</label>
-																	</div>
-											                	</li>
-											                </ul>
-											                <ul class="ui_kit_checkbox selectable-list float-right fn-400">
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck24">
-																		<label class="custom-control-label" for="customCheck24">WiFi</label>
-																	</div>
-											                	</li>
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck25">
-																		<label class="custom-control-label" for="customCheck25">Sauna</label>
-																	</div>
-											                	</li>
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck26">
-																		<label class="custom-control-label" for="customCheck26">Dryer</label>
-																	</div>
-											                	</li>
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck27">
-																		<label class="custom-control-label" for="customCheck27">Washer</label>
-																	</div>
-											                	</li>
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck28">
-																		<label class="custom-control-label" for="customCheck28">Laundry</label>
-																	</div>
-											                	</li>
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck29">
-																		<label class="custom-control-label" for="customCheck29">Outdoor Shower</label>
-																	</div>
-											                	</li>
-											                	<li>
-																	<div class="custom-control custom-checkbox">
-																		<input type="checkbox" class="custom-control-input" id="customCheck30">
-																		<label class="custom-control-label" for="customCheck30">Window Coverings</label>
-																	</div>
-											                	</li>
-											                </ul>
-												        </div>
-											        </div>
-											    </div>
-										    </div>
-										</div>
-									</li>
-							
-								</ul>
+									
+									
+									  
+									
 							</div>
 						</div>
+
+						
 						<div class="terms_condition_widget">
 							<h4 class="title">قطع الغيار</h4>
 							<div class="sidebar_feature_property_slider">
+								
 								<div class="item">
 									<div class="feat_property home7">
 										<div class="thumb">
-											<img class="img-whp" src="images/property/fp1.jpg" alt="fp1.jpg">
+											<img class="img-whp" src="images/property/fp3.jpg" alt="fp3.jpg">
 											<div class="thmb_cntnt">
 												<ul class="tag mb0">
-													<li class="list-inline-item"><a href="#">For Rent</a></li>
-													<li class="list-inline-item"><a href="#">Featured</a></li>
+													<li class="list-inline-item"><a href="#">للبيع</a></li>
+													<li class="list-inline-item"><a href="#">متوفر</a></li>
 												</ul>
-												<h4 class="posr color-white">Renovated Apartment</h4>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="item">
-									<div class="feat_property home7">
-										<div class="thumb">
-											<img class="img-whp" src="images/property/fp2.jpg" alt="fp2.jpg">
-											<div class="thmb_cntnt">
-												<ul class="tag mb0">
-													<li class="list-inline-item"><a href="#">For Rent</a></li>
-													<li class="list-inline-item"><a href="#">Featured</a></li>
-												</ul>
-												<a class="fp_price" href="#">$13,000<small>/mo</small></a>
-												<h4 class="posr color-white">Renovated Apartment</h4>
+												<a class="fp_price" href="#">10،000<small>الف ريال</small></a>
+												<h4 class="posr color-white">قطع غيار</h4>
 											</div>
 										</div>
 									</div>
@@ -592,45 +346,32 @@
 											<img class="img-whp" src="images/property/fp3.jpg" alt="fp3.jpg">
 											<div class="thmb_cntnt">
 												<ul class="tag mb0">
-													<li class="list-inline-item"><a href="#">For Rent</a></li>
-													<li class="list-inline-item"><a href="#">Featured</a></li>
+													<li class="list-inline-item"><a href="#">للبيع</a></li>
+													<li class="list-inline-item"><a href="#">متوفر</a></li>
 												</ul>
-												<a class="fp_price" href="#">$13,000<small>/mo</small></a>
-												<h4 class="posr color-white">Renovated Apartment</h4>
+												<a class="fp_price" href="#">10،000<small>الف ريال</small></a>
+												<h4 class="posr color-white">قطع غيار</h4>
 											</div>
 										</div>
 									</div>
 								</div>
+
 								<div class="item">
 									<div class="feat_property home7">
 										<div class="thumb">
-											<img class="img-whp" src="images/property/fp4.jpg" alt="fp4.jpg">
+											<img class="img-whp" src="images/property/fp3.jpg" alt="fp3.jpg">
 											<div class="thmb_cntnt">
 												<ul class="tag mb0">
-													<li class="list-inline-item"><a href="#">For Rent</a></li>
-													<li class="list-inline-item"><a href="#">Featured</a></li>
+													<li class="list-inline-item"><a href="#">للبيع</a></li>
+													<li class="list-inline-item"><a href="#">متوفر</a></li>
 												</ul>
-												<a class="fp_price" href="#">$13,000<small>/mo</small></a>
-												<h4 class="posr color-white">Renovated Apartment</h4>
+												<a class="fp_price" href="#">10،000<small>الف ريال</small></a>
+												<h4 class="posr color-white">قطع غيار</h4>
 											</div>
 										</div>
 									</div>
 								</div>
-								<div class="item">
-									<div class="feat_property home7">
-										<div class="thumb">
-											<img class="img-whp" src="images/property/fp5.jpg" alt="fp5.jpg">
-											<div class="thmb_cntnt">
-												<ul class="tag mb0">
-													<li class="list-inline-item"><a href="#">For Rent</a></li>
-													<li class="list-inline-item"><a href="#">Featured</a></li>
-												</ul>
-												<a class="fp_price" href="#">$13,000<small>/mo</small></a>
-												<h4 class="posr color-white">Renovated Apartment</h4>
-											</div>
-										</div>
-									</div>
-								</div>
+
 							</div>
 						</div>
 						<div class="terms_condition_widget">
@@ -691,29 +432,12 @@
 						<div class="grid_list_search_result">
 							<div class="col-sm-12 col-md-4 col-lg-4 col-xl-5">
 								<div class="left_area tac-xsd">
-									<p>9 Search results</p>
+									<p  style=" text-align: right; font-family:rb;"> عدد الاقاسام :9</p>
 								</div>
 							</div>
 							<div class="col-sm-12 col-md-8 col-lg-8 col-xl-7">
 								<div class="right_area text-right tac-xsd">
-									<ul>
-										<li class="list-inline-item"><span class="stts">Status:</span>
-											<select class="selectpicker show-tick">
-												<option>الجميع</option>
-												<option>جديج</option>
-												<option>مستخدم</option>
-											</select>
-										</li>
-										<li class="list-inline-item"><span class="shrtby">Sort by:</span>
-											<select class="selectpicker show-tick">
-												<option>Featured First</option>
-												<option>Featured 2nd</option>
-												<option>Featured 3rd</option>
-												<option>Featured 4th</option>
-												<option>Featured 5th</option>
-											</select>
-										</li>
-									</ul>
+									
 								</div>
 							</div>
 						</div>
